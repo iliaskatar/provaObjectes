0dd5f658ee42b9c07ef6202552f9ad11df874885
iliaskat <ikatar73@gmail.com>
2019-09-26 16:33:56 +0200

    practica_1_ex_1

---
+++ practicas_uf1/practica_1_ex_1.java
@@ -0,0 +1,16 @@
+/**
+ * practica_1_ex_1
+ */
+public class practica_1_ex_1 {
+
+    public static void main(String[] args) {
+        
+        System.out.print("Introdueix el teu nom: ");
+        String nom = System.console().readLine();
+        System.out.print("Introdueix el teu cognom:");
+        String cognom = System.console().readLine(); 
+        System.out.print("Introdueix ara el domini:");
+        String email = System.console().readLine();
+        System.out.println("el teu correu es : " + nom + "." + cognom +"@"+email);
+    }
+}
